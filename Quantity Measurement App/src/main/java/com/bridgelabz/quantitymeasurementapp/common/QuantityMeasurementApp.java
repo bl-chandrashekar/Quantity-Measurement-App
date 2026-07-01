98b6640e2ebf0d9ec1a07cfd5455f82c4bd6990b
BL-Chandrashekar <chandrashekar.kalmood@bridgelabz.com>
2026-07-01 23:40:32 +0530

    [BL-Chandrashekar] feat(UC-04): extend length unit support with yards and centimeters

--- Quantity Measurement App/src/main/java/com/bridgelabz/quantitymeasurementapp/common/QuantityMeasurementApp.java
+++ Quantity Measurement App/src/main/java/com/bridgelabz/quantitymeasurementapp/common/QuantityMeasurementApp.java
@@ -4,16 +4,21 @@ package com.bridgelabz.quantitymeasurementapp.common;
 public class QuantityMeasurementApp {
 
     public static void main(String[] args) {
-        System.out.println("Input: Quantity(1.0, \"feet\") and Quantity(12.0, \"inches\")");
-        System.out.println("Output: Equal (" + compareLength(1.0, LengthUnit.FEET, 12.0, LengthUnit.INCHES) + ")");
-
-        System.out.println("Input: Quantity(1.0, \"inch\") and Quantity(1.0, \"inch\")");
-        System.out.println("Output: Equal (" + compareLength(1.0, LengthUnit.INCHES, 1.0, LengthUnit.INCHES) + ")");
+        printResult(1.0, LengthUnit.YARDS, 3.0, LengthUnit.FEET);
+        printResult(1.0, LengthUnit.YARDS, 36.0, LengthUnit.INCHES);
+        printResult(2.0, LengthUnit.YARDS, 2.0, LengthUnit.YARDS);
+        printResult(2.0, LengthUnit.CENTIMETERS, 2.0, LengthUnit.CENTIMETERS);
+        printResult(1.0, LengthUnit.CENTIMETERS, 0.393701, LengthUnit.INCHES);
     }
 
     public static boolean compareLength(double firstValue, LengthUnit firstUnit, double secondValue, LengthUnit secondUnit) {
         QuantityLength first = new QuantityLength(firstValue, firstUnit);
         QuantityLength second = new QuantityLength(secondValue, secondUnit);
         return first.equals(second);
+    }
+
+    private static void printResult(double firstValue, LengthUnit firstUnit, double secondValue, LengthUnit secondUnit) {
+        System.out.println("Input: Quantity(" + firstValue + ", " + firstUnit + ") and Quantity(" + secondValue + ", " + secondUnit + ")");
+        System.out.println("Output: Equal (" + compareLength(firstValue, firstUnit, secondValue, secondUnit) + ")");
     }
 }
